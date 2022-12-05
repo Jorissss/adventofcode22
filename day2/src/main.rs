6b8fd6b14771853cbbb20045e9f296639e4744be
Joris den Elzen <joris@ALSI-5CG12657SH.alsi.local>
2022-12-05 09:50:53 +0100

    day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{self, BufRead};
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum RPS {
     Rock,
     Paper,
@@ -21,6 +22,23 @@ impl RPS {
             'X' => Ok(RPS::Rock),
             'Y' => Ok(RPS::Paper),
             'Z' => Ok(RPS::Scissors),
+            _ => Err(format!("{} is not a correct opponent choice!", c)),
+        }
+    }
+
+    fn from_outcome(c: &char, opponent_choice: &RPS) -> Result<Self, String> {
+        match c {
+            'X' => match opponent_choice {
+                RPS::Rock => Ok(RPS::Scissors),
+                RPS::Paper => Ok(RPS::Rock),
+                RPS::Scissors => Ok(RPS::Paper),
+            },
+            'Y' => Ok(opponent_choice.clone()),
+            'Z' => match opponent_choice {
+                RPS::Rock => Ok(RPS::Paper),
+                RPS::Paper => Ok(RPS::Scissors),
+                RPS::Scissors => Ok(RPS::Rock),
+            },
             _ => Err(format!("{} is not a correct opponent choice!", c)),
         }
     }
@@ -54,19 +72,23 @@ impl RPS {
 
 fn main() {
     let stdin = io::stdin();
-    let mut scores: u32 = 0;
+    let mut scores_part1: u32 = 0;
+    let mut scores_part2: u32 = 0;
     let mut counter = 0;
 
     for rline in stdin.lock().lines() {
         let line = rline.unwrap();
         let mut line_split = line.split_whitespace();
-        let opponent_choice =
-            RPS::from_opponent_char(&line_split.next().unwrap().chars().next().unwrap());
-        let player_choice =
-            RPS::from_player_char(&line_split.next().unwrap().chars().next().unwrap());
-        scores += player_choice.unwrap().score(&opponent_choice.unwrap());
+        let first_char = line_split.next().unwrap().chars().next().unwrap();
+        let second_char = line_split.next().unwrap().chars().next().unwrap();
+        let opponent_choice = RPS::from_opponent_char(&first_char).unwrap();
+        let player_choice_part1 = RPS::from_player_char(&second_char);
+        let player_choice_part2 = RPS::from_outcome(&second_char, &opponent_choice);
+        scores_part1 += player_choice_part1.unwrap().score(&opponent_choice);
+        scores_part2 += player_choice_part2.unwrap().score(&opponent_choice);
         counter += 1;
     }
     println!("rounds: {}", counter);
-    println!("score: {}", scores);
+    println!("score part 1: {}", scores_part1);
+    println!("score part 2: {}", scores_part2);
 }
